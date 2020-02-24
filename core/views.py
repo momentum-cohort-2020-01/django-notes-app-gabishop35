be474526cf8174c12a7767d5417537831d6d80f4
Geoff <gabishop35@gmail.com>
2020-02-24 16:49:52 -0500

    bullet points 1-3 of step 1

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpRequest
+
+import data
 
 # Create your views here.
+def index(request):
+    notes = data.NOTES
+    # breakpoint()
+    return render(request, 'base.html', {'notes': notes})
